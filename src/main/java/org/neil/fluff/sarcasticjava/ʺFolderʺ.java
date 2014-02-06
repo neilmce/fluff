94c6bbc194cf757594df55036e23f20c501889a5
neilmce <neil.mcerlean@gmail.com>
2014-02-06 17:01:59 +0000

    Added example of sarcastic Java

---
+++ src/main/java/org/neil/fluff/sarcasticjava/ʺFolderʺ.java
@@ -0,0 +1,8 @@
+package org.neil.fluff.sarcasticjava;
+
+/** I don't know what this class is, but it's not a folder. */
+public class ʺFolderʺ {
+    
+    /** I don't know what this method does, but it doesn't save. */
+    public void ʺsaveʺ() {}
+}
---
+++ src/test/java/org/neil/fluff/sarcasticjava/FolderTest.java
@@ -0,0 +1,12 @@
+package org.neil.fluff.sarcasticjava;
+
+/** I don't know what this class is, but it's not a folder. */
+public class FolderTest {
+    @Test public void useTheSarcasticTypes() throws Exception {
+        // Create a Folder
+        ʺFolderʺ f = new ʺFolderʺ();
+
+        // save the folder
+        f.ʺsaveʺ();
+    }
+}
