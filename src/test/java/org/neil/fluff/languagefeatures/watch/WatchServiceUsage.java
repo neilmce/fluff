f131bb2d8c44ebb1921939e6654903be9b1b1858
neilmce <neil.mcerlean@gmail.com>
2014-03-02 21:32:23 +0000

    Additional tests in WatchServiceUsage.

--- src/test/java/org/neil/fluff/languagefeatures/watch/WatchServiceUsage.java
+++ src/test/java/org/neil/fluff/languagefeatures/watch/WatchServiceUsage.java
@@ -7,14 +7,18 @@ import static java.nio.file.StandardWatchEventKinds.OVERFLOW;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertSame;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.nio.file.FileSystem;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardWatchEventKinds;
 import java.nio.file.WatchEvent;
+import java.nio.file.WatchEvent.Kind;
 import java.nio.file.WatchKey;
 import java.nio.file.WatchService;
 import java.util.ArrayList;
@@ -22,6 +26,7 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.CountDownLatch;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -42,6 +47,8 @@ import org.junit.rules.TemporaryFolder;
  *     See {@link #watchLargeNumberOfFolders()}.</li>
  * <li>It seems to take just under 10s (and occasionally just under 5s) to receive notification
  *     of a single file creation in a watched folder. See {@link #watchEmptyFileCreation()}</li>
+ * <li>A sequence of multiple modifications on a single file will only be reported as one
+ *     modification. See {@link #watchModifiedFile()}.</li>
  * </ul>
  */
 public class WatchServiceUsage {
@@ -65,7 +72,7 @@ public class WatchServiceUsage {
         final Path folder = createFolder("watchedFolder");
         
         // ...start watching it...
-        final WatchKey watchKey = watchFolder(folder);
+        final WatchKey watchKey = addWatch(folder);
         
         // ... create a file within that folder.
         final String fileName = "newFile.txt";
@@ -99,8 +106,97 @@ public class WatchServiceUsage {
         assertEquals(Paths.get(fileName) ,event.context());
     }
     
-    // TODO create non-empty file
-    // TODO modification (content) in a folder
+    @Test public void watchNonEmptyFileCreation() throws Exception {
+        // Create a folder...
+        final Path folder = createFolder("watchedFolder");
+        
+        // ...start watching it...
+        addWatch(folder);
+        
+        // ... create a file within that folder.
+        final String fileName = "newFile.txt";
+        createNonEmptyFile(folder, fileName);
+        
+        // ... take the next event (blocking call)...
+        WatchKey nextEvent = watcher.take();
+        
+        // Now let's look in more detail at what's in the event.
+        List<WatchEvent<?>> events = nextEvent.pollEvents();
+        
+        // There's only one event
+        assertEquals(1, events.size());
+        WatchEvent<?> event = events.get(0);
+        
+        // There should only have been one create event. (multiple similar events e.g. modifications can build up)
+        assertEquals(1, event.count());
+        
+        assertEquals(Path.class,     event.kind().type());
+        assertEquals("ENTRY_CREATE", event.kind().name());
+        
+        // The context we're given is the Path, in this case relative to the watched folder.
+        assertEquals(Paths.get(fileName) ,event.context());
+    }
+    
+    @Test public void watchModifiedFile() throws Exception {
+        // Create a folder...
+        final Path folder = createFolder("watchedFolder");
+        
+        // ... create a file within that folder.
+        final String fileName = "newFile.txt";
+        final Path file = createNonEmptyFile(folder, fileName);
+        
+        // ...start watching the folder...
+        addWatch(folder, StandardWatchEventKinds.ENTRY_MODIFY);
+        
+        // Simply modifying the file at this point never produces a modification event on Mac OS X 10.7 with Java 7.
+        // I wonder is this because file modification detection is based on filesystem polling? If it were, the system
+        // would need to be polled twice to see any changes and the immediate modification of a file could predate the first
+        // poll?
+        //
+        // In any case, we'll trigger a series of modifications and see how long the WatchService takes to react - if indeed
+        // it does react - and then how many modification events it reports.
+        final int modCount = 10;
+        final CountDownLatch latch = new CountDownLatch(modCount);
+        Runnable modifyFile = new Runnable() {
+            @Override public void run() {
+                for (int i = 0; i < modCount; i++) {
+                    try {
+                        modifyFile(file, "modified file " + i);
+                        latch.countDown();
+                    } catch (IOException ignored) {
+                        // Intentionally empty
+                    }
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException ignored) {
+                        Thread.interrupted();
+                    }
+                }
+            }
+        };
+        new Thread(modifyFile).start();
+        latch.await();
+        
+        // ... take the next event (blocking call)...
+        WatchKey nextEvent = watcher.take();
+        
+        // Now let's look in more detail at what's in the event.
+        List<WatchEvent<?>> events = nextEvent.pollEvents();
+        
+        // There's only one event
+        assertEquals(1, events.size());
+        WatchEvent<?> event = events.get(0);
+        
+        // Note that afte 10 modifications, only 1 has been detected. (file polling?)
+        assertEquals(1, event.count());
+        
+        assertEquals(Path.class,     event.kind().type());
+        assertEquals("ENTRY_MODIFY", event.kind().name());
+        
+        // The context we're given is the Path, in this case relative to the watched folder.
+        assertEquals(Paths.get(fileName) ,event.context());
+    }
+    
     // TODO rename in a folder
     // TODO delete in a folder
     // TODO modification (rename) of watched folder
@@ -123,6 +219,7 @@ public class WatchServiceUsage {
     // TODO file permissions - can user see changes on files they have no permission to see? Or rather: what happens?
     // TODO Load tests - large numbers of creates, deletes, modifications-to-single-file, modifications-to-many-files - within one WatchKey.
     // TODO unwatching while events are pending?
+    // TODO There's a watch kind of OVERFLOW. How do we hit it?
     
     /** This test method checks that it is possible to watch large numbers of folders. */
     @Ignore("Ignored because it generates a lot of logging, but this passes just fine.")
@@ -135,7 +232,7 @@ public class WatchServiceUsage {
         for (int i = 0; i < folderCount; i++) {
             final Path folder = createFolder("watchedFolder" + i);
             
-            watchedFolders.put(folder, watchFolder(folder));
+            watchedFolders.put(folder, addWatch(folder));
         }
         
         // Then create an empty file in each of them.
@@ -156,9 +253,14 @@ public class WatchServiceUsage {
         assertEquals(folderCount, eventsReceived);
     }
     
-    private WatchKey watchFolder(final Path folder) throws IOException {
-        LOG.debug("Adding watch to folder " + folder);
-        return folder.register(watcher, ENTRY_CREATE, ENTRY_DELETE, ENTRY_MODIFY, OVERFLOW);
+    private WatchKey addWatch(final Path path) throws IOException {
+        LOG.debug("Adding watch to " + path);
+        return path.register(watcher, ENTRY_CREATE, ENTRY_DELETE, ENTRY_MODIFY, OVERFLOW);
+    }
+    
+    private WatchKey addWatch(final Path path, Kind<Path>... watchKinds) throws IOException {
+        LOG.debug("Adding watch to " + path);
+        return path.register(watcher, watchKinds);
     }
     
     private Path createEmptyFile(Path parentFolder, String fileName) throws IOException {
@@ -167,6 +269,23 @@ public class WatchServiceUsage {
         LOG.debug("Created empty file: " + file);
         
         return file;
+    }
+    
+    private Path createNonEmptyFile(Path parentFolder, String fileName) throws IOException {
+        Path file = createEmptyFile(parentFolder, fileName);
+        
+        modifyFile(file, "Newly created file.");
+        
+        return file;
+    }
+    
+    private void modifyFile(Path file, String content) throws IOException {
+        LOG.debug("Modifying content in file: " + file);
+        try (BufferedWriter w = Files.newBufferedWriter(file, Charset.defaultCharset())) {
+            w.append(content);
+            w.newLine();
+            w.flush();
+        }
     }
     
     /** Creates a folder in the Java temporary folder, giving it a random name. */
